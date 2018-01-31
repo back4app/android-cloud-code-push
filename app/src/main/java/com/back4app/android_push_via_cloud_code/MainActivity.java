fc2454d7e8bebf0a33ec800d2ec3a54b2532205b
Igor Ribeiro <ribeiro_igor@hotmail.com>
2018-01-31 14:01:45 -0200

    Removing unnecessary imports

--- app/src/main/java/com/back4app/android_push_via_cloud_code/MainActivity.java
+++ app/src/main/java/com/back4app/android_push_via_cloud_code/MainActivity.java
@@ -3,7 +3,6 @@ package com.back4app.android_push_via_cloud_code;
 import android.app.AlertDialog;
 import android.app.ProgressDialog;
 import android.content.DialogInterface;
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -11,12 +10,10 @@ import android.widget.Button;
 import android.widget.Toast;
 
 import com.parse.FunctionCallback;
-import com.parse.LogInCallback;
 import com.parse.Parse;
 import com.parse.ParseCloud;
 import com.parse.ParseException;
 import com.parse.ParseInstallation;
-import com.parse.ParseUser;
 
 import java.util.ArrayList;
 import java.util.HashMap;
